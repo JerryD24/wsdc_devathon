176b2cc9bc3f895adba6de506b4aa8933f24d552
Prabhat <prabhatc7879@gmail.com>
2021-09-19 01:14:19 +0530

    front update

--- registration.php
+++ registration.php
@@ -15,13 +15,15 @@
   $docs=$_POST['docs'];
   $image=$_POST['image'];
 
-  $stat_check="SELECT stat_app FROM wsdc_table";
+  $stat_check="SELECT stat_app FROM wsdc_table";     //aaaaaa
+  mysqli_query($con,$stat_check);
   
   if(!$stat_check){
-  $inqry="INSERT INTO wsdc_table(name,adhar,address,gender,department,specialization,category,PWD,date,notes,docs,image,app_stat) VALUES ('$nm','$adhar','$address','gender','depart','specialization','category','pwd','date','notes','docs','image','true')";
+  $inqry="INSERT INTO wsdc_table(name,adhar,address,gender,department,specialization,category,PWD,date,notes,docs,image,app_stat) VALUES ('$nm','$adhar','$address','$gender','$depart','$specialization','$category','$pwd','$date','$notes','$docs','$image','true')";
   mysqli_query($con,$inqry);
   $change_stat="UPDATE wsdc_table SET stat_app='true'";
   mysqli_query($con,$change_stat);
+  header('location:index.html');
   }
   else
   header('location:landing page.html');
