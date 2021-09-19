0be6c4a7e760ece9c1d64ae33d6ee8b19f01df6f
Prabhat <prabhatc7879@gmail.com>
2021-09-19 11:57:10 +0530

    kk

--- show.php
+++ show.php
@@ -1,4 +1,22 @@
+<?php
+  include "./connection.php";
 
+  if(isset($_POST['submit']))
+  {
+    $adhar=$_POST['adhar'];
+    
+  
+      $selqry="SELECT * FROM wsdc_table WHERE adhar='$adhar'";
+      $qry=mysqli_query($con,$selqry);
+      $count=mysqli_num_rows($qry);
+      if(!$count){
+      echo "Incorrect data";
+      }
+      else
+      {
+        $data=mysqli_fetch_array($qry);
+      }  
+  ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,7 +28,16 @@
 <body>
   <h2>your data</h2>
   <p>
-    
+    <h4>Name:<?php echo $data["name"];?></h4><br>
+    <h4>Adhar:<?php echo $data["adhar"];?></h4><br>
+    <h4>Address:<?php echo $data["address"];?></h4><br>
+    <h4>Department:<?php echo $data["department"];?></h4><br>
+    <h4>Specialization:<?php echo $data["specialization"];?></h4><br>
+    <h4>Category:<?php echo $data["category"];?></h4><br>
+    <h4>PWD:<?php echo $data["PWD"];?></h4><br>
+    <h4>Date:<?php echo $data["date"];?></h4><br>
+    <h4>Notes:<?php echo $data["notes"];?></h4><br>
+    <h4>Status:<?php echo $data["stat_app"];?></h4><br>
   </p>
   <button><a href="logout.php">Log out</a></button>
 </body>
