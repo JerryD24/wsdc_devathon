9533ab488d8a2a0d87629441787aa455c0565fed
Prabhat Singh Rajput <78310049+JerryD24@users.noreply.github.com>
2022-06-30 21:52:08 +0530

    updating login page

--- login.php
+++ login.php
@@ -9,15 +9,13 @@
     $selqry="SELECT * FROM admin_table WHERE email='$mail'";
     $qry=mysqli_query($con,$selqry);
     $count=mysqli_num_rows($qry);
-    $admin="admin";       //for sake of simplicity
-    if($mail!=$admin){                 //altered if(!$count){
+    if(!$count){ 
     echo "Incorrect data";
     }
     else
     {
-      //$res=mysqli_fetch_array($qry);
-      $res="admin";
-      if($res==$ps){    //altered if($res['password']==$ps){
+      $res=mysqli_fetch_array($qry);
+      if($res['password']==$ps){
         $_SESSION['admin']=1;
         echo "inside admin";
       header('location:admin.php');}
